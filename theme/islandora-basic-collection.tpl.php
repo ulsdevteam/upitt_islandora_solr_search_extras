54625891839340dabb7083f2ca9af33a69260c2f
Brian Gillingham <bgilling@pitt.edu>
2016-07-14 09:41:09 -0400

    more big changes needed to get this working under islandora_default theme

--- theme/islandora-basic-collection.tpl.php
+++ theme/islandora-basic-collection.tpl.php
@@ -1,4 +1,4 @@
-<?php
+hello world!?<?php
 
 /**
  * @file
---
+++ theme/islandora-objects-search-results.tpl.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * islandora-objects-list.tpl.php
+ *
+ * Render a bunch of objects in a list or grid view.
+ */
+?>
+<div class="islandora-objects-list">
+<?php if ($solr_search) : ?>
+  <div>
+    <?php print $solr_search; ?>
+  </div>
+<?php endif; ?>
+</div>
