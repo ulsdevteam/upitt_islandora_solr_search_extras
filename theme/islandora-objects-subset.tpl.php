33ea8c2afe771129f77ce230134347cadb34a668
Brian Gillingham <bgilling@pitt.edu>
2016-07-19 10:30:23 -0400

    fine tuning templates and css

--- theme/islandora-objects-subset.tpl.php
+++ theme/islandora-objects-subset.tpl.php
@@ -40,9 +40,6 @@
     ?>
   </span>
     <?php print $pager; ?>
-    <?php if ($total_count) : ?>
-    <div>Total objects: <?php print $total_count; ?></div>
-    <?php endif; ?>
 
     <?php print $content; ?>
     <?php print $pager; ?>
