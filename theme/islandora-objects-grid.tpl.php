36b00a17a361fb34e13e0e60c3c1c588c4af9200
Brian Gillingham <bgilling@pitt.edu>
2016-08-04 11:18:04 -0400

    removed comments from teplates

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -11,23 +11,3 @@
 <?php if ($solr_search) : ?>
   <div><?php print $solr_search; ?></div>
 <?php endif; ?>
-
-
-
-
-
-
-<?php /*
-< ?php if (isset($collection_search)) {
-  print render($collection_search);
-} ? >   
-<div class="islandora-objects-grid clearfix">
- < ?php foreach($objects as $object): ?>
-   <div class="islandora-objects-grid-item">
-     <dl class="islandora-object < ?php print $object['class']; ? >">
-       <dt class="islandora-object-thumb">< ?php print $object['thumb']; ? ></dt>
-       <dd class="islandora-object-caption">< ?php print $object['link']; ? ></dd>
-     </dl>
-   </div>
- < ?php endforeach; ? >
-</div>
--- theme/islandora-objects-list.tpl.php
+++ theme/islandora-objects-list.tpl.php
@@ -6,18 +6,8 @@
  */
 ?>
 <!-- template islandora-objects-list.tpl.php -->
-<!-- template islandora-objects-grid.tpl.php -->
 <?php print $whamjack_sidebar; ?>
 
 <?php if ($solr_search) : ?>
   <div><?php print $solr_search; ?></div>
 <?php endif; ?>
-
-<?php /*
-< ?php if (isset($collection_search)) {
-  print render($collection_search);
-} ? >   
-< ?php if ($solr_search) : ? >
-  <div>< ?php print $solr_search; ? ></div>
-< ?php endif; ? >
-
