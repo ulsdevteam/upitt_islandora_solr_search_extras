36530bb3a17efe576287010aef7ef824497dd12c
Brian Gillingham <bgilling@pitt.edu>
2016-08-18 09:17:30 -0400

    more adjustments to get search block working in sidebar for non-islandora/search pages

---
+++ js/solr_extras.js
@@ -0,0 +1,31 @@
+/**
+ * @file
+ * Javascript file for islandora solr extras
+ */
+
+(function ($) {
+
+   $(document).ready(function() {
+     // Simply set all search boxes to empty strings.
+     $('#islandora-solr-advanced-terms').children("div").each(function(i, obj) {
+console.log(obj);
+//       obj.children("input .form-text").val('');
+     });
+
+/*
+
+     // Remove the supplemental search term blocks and the "remove" button for the first search term.
+     $('#edit-terms-0-remove').remove();
+     while ($('#islandora-solr-advanced-terms > div').length > 1) {
+       $('#islandora-solr-advanced-terms').children("div:last").remove();
+     }
+     // Now, with the last div remaining, remove the last div in there to get rid of the "AND" / "OR" selector 
+     $('#islandora-solr-advanced-terms').children("div").children("div:last").remove();
+
+     // Empty the search box
+     $('#edit-terms-0-search').val('');
+*/
+
+   });
+})(jQuery);
+
