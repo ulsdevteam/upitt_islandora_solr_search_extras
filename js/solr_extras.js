32f42d163e5480ae735f25aac2fb324caa43fd70
Brian Gillingham <bgilling@pitt.edu>
2016-09-07 16:14:09 -0400

    million changes to get collection page to function as a solr search page

--- js/solr_extras.js
+++ js/solr_extras.js
@@ -10,6 +10,13 @@
      // Remove those individual + / - facet links
      $('.islandora-solr-facet li .plusminus').remove();
 
+     $('#islandora-solr-advanced-terms div').find('input:text').each(function() {
+       text_val = $(this).val();
+       if (text_val.indexOf('nfo\\:fedora/pitt') > 0) {
+         $(this).val('');
+       }
+     });
+
    });
 })(jQuery);
 
--- theme/islandora-solr-wrapper.tpl.php
+++ theme/islandora-solr-wrapper.tpl.php
@@ -1,31 +1,48 @@
-<?php
-/**
- * @file
- * Islandora solr search results wrapper template
- *
- * Variables available:
- * - $variables: all array elements of $variables can be used as a variable.
- *   e.g. $base_url equals $variables['base_url']
- * - $base_url: The base url of the current website. eg: http://example.com .
- * - $user: The user object.
- *
- * - $secondary_profiles: Rendered secondary profiles
- * - $results: Rendered search results (primary profile)
- * - $islandora_solr_result_count: Solr result count string
- * - $solrpager: The pager
- * - $solr_debug: debug info
- *
- * @see template_preprocess_islandora_solr_wrapper()
- */
-?>
+<!-- template islandora-solr-wrapper.tpl.php -->
+<?php $is_first_page = !(isset($_GET['page'])); ?>
+<?php if (isset($messages)) { print $messages; } ?>
 
-<div id="islandora-solr-top">
-  <?php print $secondary_profiles; ?>
-  <div id="islandora-solr-result-count"><?php print $islandora_solr_result_count; ?></div>
-</div>
+  <?php if (isset($tabs)): ?>
+  <div class="tabs"><?php print render($tabs); ?></div>
+  <?php endif; ?>
+  <?php if (isset($action_links)): ?>
+  <ul class="action-links">
+    <?php print render($action_links); ?>
+  </ul>
+  <?php endif; ?>
+
+  <?php if (isset($collection_page_metadata) && $is_first_page): ?>
+    <div id="metadata-sidebar" style="width: 33%; float: right; margin-left: 20px;">
+      <div class="islandora-basic-collection-metadata">
+      <?php print $collection_page_metadata; ?> 
+      </div>
+    </div>
+  <?php endif; ?>
+    <?php print render($title_prefix); ?>
+    <?php if (isset($collection_tn_img) && $is_first_page): ?>
+      <div class="islandora-basic-collection-metadata-image"> <?php print $collection_tn_img; ?> </div>
+    <?php endif; ?>
+
+  <?php
+    if ($is_first_page && $is_collection) {
+      $block = block_load('islandora_datastream_blocks', 'idb-DESC');
+      $renderable_block = _block_get_renderable_array(_block_render_blocks(array($block)));
+      print $renderable_block['islandora_datastream_blocks_idb-DESC']['#markup'];
+    }
+  ?>
+
 <div class="islandora-solr-content">
+  <div id="islandora-solr-top">
+    <?php print $secondary_profiles; ?>
+    <div id="islandora-solr-result-count">
+      <?php print $islandora_solr_result_count; ?>
+    </div>
+  </div>
   <?php print $solr_pager; ?>
   <?php print $results; ?>
   <?php print $solr_debug; ?>
   <?php print $solr_pager; ?>
 </div>
+
+</div><!-- /end main-content -->
+
