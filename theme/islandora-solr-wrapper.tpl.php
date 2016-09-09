f1b16933fc59d5c1eed4ef26f57bd1a2561e6d0a
Brian Gillingham <bgilling@pitt.edu>
2016-09-09 12:46:23 -0400

    added remaining format rules for alias generation for collections, adjusted template for web, removed die() stmt

--- theme/islandora-solr-wrapper.tpl.php
+++ theme/islandora-solr-wrapper.tpl.php
@@ -1,5 +1,4 @@
 <!-- template islandora-solr-wrapper.tpl.php -->
-<?php $is_first_page = !(isset($_GET['page'])); ?>
 <?php if (isset($messages)) { print $messages; } ?>
 
   <?php if (isset($tabs)): ?>
@@ -19,17 +18,6 @@
     </div>
   <?php endif; ?>
     <?php print render($title_prefix); ?>
-    <?php if (isset($collection_tn_img) && $is_first_page): ?>
-      <div class="islandora-basic-collection-metadata-image"> <?php print $collection_tn_img; ?> </div>
-    <?php endif; ?>
-
-  <?php
-    if ($is_first_page && $is_collection) {
-      $block = block_load('islandora_datastream_blocks', 'idb-DESC');
-      $renderable_block = _block_get_renderable_array(_block_render_blocks(array($block)));
-      print $renderable_block['islandora_datastream_blocks_idb-DESC']['#markup'];
-    }
-  ?>
 
 <div class="islandora-solr-content">
   <div id="islandora-solr-top">
